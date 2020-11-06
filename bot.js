c58c74d0982899d9c562624e74118e0bd32d2542
claraharguindey <clara.harguindey@secuoyas.com>
2020-11-06 19:36:04 +0100

    update with date

--- bot.js
+++ bot.js
@@ -1,22 +1,22 @@
-const config = require('./config');
-const tweets = require('./tweets');
-const Twit = require('twit');
-const T = new Twit(config)
+const config = require("./config");
+const tweets = require("./tweets");
+const Twit = require("twit");
+const T = new Twit(config);
 
 const randomNumber = Math.floor(Math.random() * tweets.length);
 
 tweet();
-setInterval(tweet, 1000*43200);
-
+setInterval(tweet, 1000 * 43200);
 
 function tweet() {
-    T.post('statuses/update', { status: tweets[randomNumber] }, tweeted);
+  const currentTweet = tweets[randomNumber];
+  T.post("statuses/update", { status: currentTweet }, tweeted);
 
-    tweets.splice(randomNumber, 1);
+  tweets.splice(randomNumber, 1);
 
-    function tweeted(err, data, response) {
-        err
-        ? console.log('Something went wrong!', err)
-        : console.log('It worked!');
-    }
+  function tweeted(err, data, response) {
+    err
+      ? console.log("Something went wrong!🥵", err)
+      : console.log(`It worked! ✨ ${currentTweet} ✨`);
+  }
 }
--- tweets.js
+++ tweets.js
@@ -1,34 +1,47 @@
 const symbol = [
-    `🙁`,
-    `🌧`,
-    `🌃`,
-    `.`,
-    `💔`,
-    `...`,
-    `😔`,
-    `👊🏿`,
-    `🤜🏻 `,
-    `🔥`,
-    `🌪`,
-    `🕳️ `,
-    `  `,
-    `🙇‍♀️`,
-    `..`,
-    `༄`,
-    `...`,
-    `ส็็็็็็็็็้้้้้้้้้้้้้ ส้้้้้้้้้้็็็็็็็็็็็็ 
+  `🙁`,
+  `🌧`,
+  `🌃`,
+  `.`,
+  `💔`,
+  `...`,
+  `😔`,
+  `👊🏿`,
+  `🤜🏻 `,
+  `🔥`,
+  `🌪`,
+  `🕳️ `,
+  `  `,
+  `🙇‍♀️`,
+  `..`,
+  `༄`,
+  `...`,
+  `ส็็็็็็็็็้้้้้้้้้้้้้ ส้้้้้้้้้้็็็็็็็็็็็็ 
     ส้้้้้้้้้้็็็็็็็็็็็็`,
-    `░҉҉҉҉҉҉҉҉҉҉҉⃝`,
-]
+  `░҉҉҉҉҉҉҉҉҉҉҉⃝`,
+];
 
-const randomNumber = Math.floor(Math.random() * symbol.length);
-const randomSymbol = symbol[randomNumber];
+const spacing = [" ", "  ", "   "];
+
+const randomSymbolIndex = Math.floor(Math.random() * symbol.length);
+const randomSpacingIndex = Math.floor(Math.random() * spacing.length);
+const randomSymbol = symbol[randomSymbolIndex];
+const randomSpacing = spacing[randomSpacingIndex];
+
+let today = new Date();
+const dd = String(today.getDate()).padStart(2, "0");
+const mm = String(today.getMonth() + 1).padStart(2, "0");
+const yyyy = today.getFullYear();
+
+today = dd + "/" + mm + "/" + yyyy;
 
 const tweets = [
-    `No hay queja ${randomSymbol}`,
-    `Estamos investigando futuras protestas${randomSymbol}.`,
-    `${randomSymbol}𝖊𝖘𝖙𝖚𝖉𝖎𝖆𝖓𝖉𝖔 𝖕𝖗𝖔𝖙𝖊𝖘𝖙𝖆𝖘`,
-    `𝖆𝖍𝖔𝖗𝖆, 𝖆𝖍𝖔𝖗𝖆, 𝖆𝖍𝖔𝖗𝖆, PROTESTO`
-]
+  `no hay queja${randomSpacing}`,
+  `(estamos investigando futuras protestas${randomSymbol})`,
+  `¡estoy harta!
+   (esta queja se escribió el día 6/11/2020 y se publicó el día ${today})`,
+  `${randomSymbol}𝖊𝖘𝖙𝖚𝖉𝖎𝖆𝖓𝖉𝖔 𝖕𝖗𝖔𝖙𝖊𝖘𝖙𝖆𝖘`,
+  `𝖆𝖍𝖔𝖗𝖆, 𝖆𝖍𝖔𝖗𝖆, 𝖆𝖍𝖔𝖗𝖆,${randomSpacing}PROTESTO`,
+];
 
 module.exports = tweets;
